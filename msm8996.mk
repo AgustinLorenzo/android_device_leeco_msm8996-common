02e67ecd9affd25c78101b77a2646bb2dedecc9e
shivatejapeddi <shivatejapeddi@gmail.com>
2019-03-04 13:01:03 +0000

    msm8996 : Rework Per App thermal configs for older chips

--- msm8996.mk
+++ msm8996.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     DeviceSettings
 
+# Thermal
+PRODUCT_PACKAGES += \
+    ThermalController
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
